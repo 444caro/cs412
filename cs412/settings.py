1b3743795c83179c54e85c4cfe05a5abe7dbfbd2
Caroline <cjm72@bu.edu>
2024-09-22 23:48:37 -0400

    minor fixes

--- cs412/settings.py
+++ cs412/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'quotes_hard.apps.QuotesHardConfig'
+    'quotes'
 ]
 
 MIDDLEWARE = [
