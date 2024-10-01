e44754c4f7c539bcce719f639d872363c25648f8
Caroline <cjm72@bu.edu>
2024-10-01 12:01:29 -0400

    blog finsished

--- blog/models.py
+++ blog/models.py
@@ -9,6 +9,7 @@ class Article(models.Model):
     author = models.TextField(blank = False)
     text = models.TextField(blank = False)
     published = models.DateTimeField(auto_now = True)  #when object is created, it will automatically set the current time
+    image_url = models.URLField(blank=True)   #true means optional field
     
     def __str__(self):
         '''return a string representation of the object'''
