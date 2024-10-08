f75ed8666bc2204b69fce682d71d77479d2b5a44
Caroline <cjm72@bu.edu>
2024-10-08 16:21:09 -0400

    class example 6.2

--- blog/models.py
+++ blog/models.py
@@ -14,3 +14,21 @@ class Article(models.Model):
     def __str__(self):
         '''return a string representation of the object'''
         return f'{self.title} by {self.author}'
+
+class Comment(models.Model):
+    '''Encapsulate the idea of a Comment on an Article.'''
+    
+    # data attributes of a Comment:
+    article = models.ForeignKey("Article", on_delete=models.CASCADE)
+    author = models.TextField(blank=False)
+    text = models.TextField(blank=False)
+    published = models.DateTimeField(auto_now=True)
+    
+    def __str__(self):
+        '''Return a string representation of this Comment object.'''
+        return f'{self.text}'
+    
+    def get_comments(self):
+        '''Return all of the comments about this article.'''
+        comments = Comment.objects.filter(article=self)
+        return comments
