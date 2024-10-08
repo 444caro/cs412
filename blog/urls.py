e48b50f1a6052c800d150923920b2d99d2a039be
Caroline <cjm72@bu.edu>
2024-10-08 16:10:21 -0400

    class example 6.1

--- blog/urls.py
+++ blog/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
 from django.conf import settings
 from . import views
+from .views import *
 
 
 urlpatterns=[
-    path(r'', views.ShowAllView.as_view(), name="show_all"),
+    path('', RandomArticleView.as_view(), name = 'random'),
+    path('show_all', ShowAllView.as_view(), name='show_all'),
+    path('article/<int:pk>', ArticlePageView.as_view(), name='article')
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,9 @@
 # blog/views.py`
 # define the views for the blog app`
 from django.shortcuts import render
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 from .models import *
+import random
 
 # Create your views here.
 
@@ -13,3 +14,20 @@ class ShowAllView(ListView):
     template_name = 'blog/show_all.html' #the template to use
     context_object_name = 'articles' #model describes one article, so we use the plural form for the context variable 
     
+class RandomArticleView(DetailView):
+    '''Show the details for one article'''
+    model = Article #the model to display
+    template_name = 'blog/article.html' #the template to use
+    context_object_name = 'article' #model describes one article, so we use the singular form for the context variable
+    
+    #pick one article at random
+    def get_object(self):
+        '''return one random article'''
+        all_articles = Article.objects.all()
+        return random.choice(all_articles)
+    
+class ArticlePageView(DetailView):
+    '''Show the details for one article'''
+    model = Article #the model to display
+    template_name = 'blog/article.html' #the template to use
+    context_object_name = 'article' #model describes one article, so we use the singular form for the context variable
