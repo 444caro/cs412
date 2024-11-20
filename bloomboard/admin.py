777d6fb76e6358a33b153cb858a82aa6a1a37a43
Caroline <cjm72@bu.edu>
2024-11-20 14:15:42 -0500

    added data

--- bloomboard/admin.py
+++ bloomboard/admin.py
@@ -1,3 +1,11 @@
+# bloomboard/admin.py
+# tell the admin we want to administer these models 
 from django.contrib import admin
+from .models import *
 
 # Register your models here.
+admin.site.register(Flower)
+admin.site.register(Vase)
+admin.site.register(BBProfile)
+admin.site.register(Post)
+admin.site.register(Comment)
